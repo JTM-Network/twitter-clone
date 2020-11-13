97cf450ff33d61fd4383c5e15b87999c27d88da8
Joe Matthews <joetymatthews@gmail.com>
2020-11-13 06:05:39 +0000

    New: Initial service projects

---
+++ Backend/feed-service/src/main/java/com/jtm/feed/FeedService.java
@@ -0,0 +1,12 @@
+package com.jtm.feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedService.class, args);
+    }
+}
---
+++ Backend/profile-service/src/main/java/com/jtm/profile/ProfileService.java
@@ -0,0 +1,12 @@
+package com.jtm.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileService.class, args);
+    }
+}
